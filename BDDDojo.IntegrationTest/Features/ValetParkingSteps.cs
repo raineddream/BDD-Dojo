dc52b8b9b8d757decc70176a6bde948e03cb503b
raineddream <raineddream@gmail.com>
2014-08-20 16:37:13 +0800

    Add Integration Test: Calculate Valet Parking Cost for half an hour

---
+++ BDDDojo.IntegrationTest/Features/ValetParkingSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Rain.BDDDojo.IntegrationTest.Features
+{
+    [Binding]
+    public class ValetParkingSteps
+    {
+        [When(@"I park my car in the Valet Parking Lot for (.*) minutes")]
+        public void When_I_par_my_car_in_the_valet_parking_lot_for_minutes(int minutes)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I will have to pay \$(.*)")]
+        public void ThenIWillHaveToPay(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
