9964433aab36a34d31d5389203637d7f6c5149a1
raineddream <raineddream@gmail.com>
2014-08-20 18:23:08 +0800

    Supply unit test: Should charge 2.00 when valet parking time is less than 5 hours.

---
+++ BDDDojo.UnitTest/Calculators/ValetParkingCalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Rain.BDDDojo.ParkingCost;
+using Rain.BDDDojo.ParkingCost.Calculators;
+
+namespace BDDDojo.UnitTest.Calculators
+{
+    [TestFixture]
+    public class ValetParkingCalculatorTest
+    {
+        private IParkingCostCalculator _costCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _costCalculator = CalculatorFactory.CreateCalculator(ParkingType.ValetParking);
+        }
+
+        [Test]
+        public void Should_charge_12_dollars_when_valet_parking_time_is_less_than_5_hours()
+        {
+            Assert.That(_costCalculator.CalculateCost(30), Is.EqualTo(12));
+        }
+    }
+}
