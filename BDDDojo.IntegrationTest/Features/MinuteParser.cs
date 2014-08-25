440565925a05d67107316254e9ce815c9bbb1bc0
raineddream <raineddream@gmail.com>
2014-08-25 13:54:02 +0800

    Minute parsing: only parse X minutes

---
+++ BDDDojo.IntegrationTest/Features/MinuteParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Rain.BDDDojo.IntegrationTest.Features
+{
+    public class MinuteParser
+    {
+        public static int Parse(string duration)
+        {
+            Match match = Regex.Match(duration, @"(\d+)\sminutes");
+            if (match.Success)
+            {
+                string minText = match.Groups[1].Value;
+                return int.Parse(minText);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ BDDDojo.IntegrationTest/SelfTest/MinuteParserTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Rain.BDDDojo.IntegrationTest.Features;
+
+namespace Rain.BDDDojo.IntegrationTest.SelfTest
+{
+    [TestFixture]
+    public class MinuteParserTest
+    {
+        [Test]
+        public void Should_only_parse_minutes()
+        {
+            int minutes = MinuteParser.Parse("10 minutes");
+
+            Assert.That(minutes, Is.EqualTo(10));
+        }
+    }
+}
